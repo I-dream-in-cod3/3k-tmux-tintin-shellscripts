ae07812e74ebdf91230cb051668d355234035214
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2026-02-07 11:23:24 -0500

    Add script to manage tmux session for cbot

---
+++ resume_cbot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+
+session="cbot"
+
+# Check if the session exists, discarding output
+# We can check $? for the exit status (zero for success, non-zero for failure)
+
+ tmux has-session -t $session 2>/dev/null
+
+if [ $? == 0 ]; then
+
+tmux send-keys -t $session ^C
+
+tmux send-keys -t $session "start" Enter
+
+else
+    echo no active session called $session
+fi
+
+# Attach to created session
+#   tmux attach-session -t $session
+#   not needed since
